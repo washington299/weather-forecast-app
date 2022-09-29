be64b51ac578765a8622ae633252873e304cbf63
Washington Campos <washingtoncampos9@gmail.com>
2022-09-29 18:27:33 -0300

    chore: add and setup axios and react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from 'App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/services/queries.ts
@@ -0,0 +1,7 @@
+import { api } from 'services/config';
+
+export const getCityLatAndLon = (city: string) =>
+	api.get(`/geo/1.0/direct?q=${city}&limit=1&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`);
+
+export const getCityWheater = (lat: number, lon: number) =>
+	api.get(`/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`)
