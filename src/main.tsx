d9d86384aff8376f2ae6a24ff30326b16bba7b34
Washington Campos <washingtoncampos9@gmail.com>
2022-09-29 18:59:19 -0300

    chore: set up chakra global styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from 'App';
 
+import { theme } from 'styles/config';
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<ChakraProvider>
+			<ChakraProvider theme={theme}>
 				<App />
 			</ChakraProvider>
 		</QueryClientProvider>
