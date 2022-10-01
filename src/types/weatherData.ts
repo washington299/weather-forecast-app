d0ccc0e2907d9c86806b4a988ec073f363088658
Washington Campos <washingtoncampos9@gmail.com>
2022-10-01 14:05:35 -0300

    feat: create data types

---
+++ src/types/weatherData.ts
@@ -0,0 +1,44 @@
+type Main = {
+	temp: number;
+	feels_like: number;
+	temp_min: number;
+	temp_max: number;
+	pressure: number;
+	sea_level: number;
+	grnd_level: number;
+	humidity: number;
+	temp_kf: number;
+}
+
+type Weather = {
+	id: number;
+	main: string;
+	description: string;
+	icon: string;
+}
+
+type Clouds = {
+	all: number;
+}
+
+type Wind = {
+	speed: number;
+	deg: number;
+	gust: number;
+}
+
+type Sys = {
+	pod: string;
+}
+
+export type WeatherDataProps = {
+	dt: number;
+	main: Main;
+	weather: Weather[];
+	clouds: Clouds;
+	wind: Wind;
+	visibility: number;
+	pop: number;
+	sys: Sys;
+	dt_txt: string;
+}
