261c0a96c0d8f446989ce18c0d00eb2b4ab920db
Washington Campos <washingtoncampos9@gmail.com>
2022-10-01 15:37:41 -0300

    refactor: add units param on getCityWeather url

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -4,4 +4,4 @@ export const getCityLatAndLon = (city: string) =>
 	api.get(`/geo/1.0/direct?q=${city}&limit=1&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`);
 
 export const getCityWheater = (lat: number, lon: number) =>
-	api.get(`/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`)
+	api.get(`/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`)
