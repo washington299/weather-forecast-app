5a54bcc1c0b267aa433d33832cdfb2bbe51edf32
Washington Campos <washingtoncampos9@gmail.com>
2022-10-01 15:38:48 -0300

    feat: add weather data on WeatherSidebar

--- src/components/WeatherSidebar/index.tsx
+++ src/components/WeatherSidebar/index.tsx
@@ -1,17 +1,43 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, HStack, VStack, Text, Image } from "@chakra-ui/react";
 import { useRecoilValue } from 'recoil';
 
 import { cityNameState } from 'state/atoms';
 
+import { getWeekDay } from 'utils/formatDate';
+
 import { WeatherSidebarProps } from './types';
 
-export const WeatherSidebar = ({ isLoading }: WeatherSidebarProps) => {
+export const WeatherSidebar = ({ isLoading, data }: WeatherSidebarProps) => {
 	const cityName = useRecoilValue(cityNameState);
 
 	return (
-		<Box w={{ base: 'full', lg: '300px' }} p={10} bg="blue.800" borderRadius="3xl">
+		<Box w={{ base: 'full', lg: '350px' }} p={4} bg="blue.800" borderRadius="3xl">
 			{isLoading && <Text>Loading...</Text>}
 			<Text>{cityName}</Text>
+
+			{data.map((weatherInfo) => {
+				const { temp_min, temp_max } = weatherInfo.main;
+				const { description, icon } = weatherInfo.weather[0];
+				const day = new Date(weatherInfo.dt_txt).getDay();
+
+				return (
+					<HStack key={day} justifyContent="space-between">
+						<HStack>
+							<Image
+								src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+								alt={description}
+								w="60px"
+								h="60px"
+							/>
+							<VStack alignItems="normal" spacing={0}>
+								<Text fontSize="xs">{getWeekDay(day)}</Text>
+								<Text>{description}</Text>
+							</VStack>
+						</HStack>
+						<Text>{`${Math.round(temp_min)}° / ${Math.round(temp_max)}°`}</Text>
+					</HStack>
+				);
+			})}
 		</Box>
 	);
 };
