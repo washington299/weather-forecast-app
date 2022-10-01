d99f246debbb41527189fca44b44f41c6955aee8
Washington Campos <washingtoncampos9@gmail.com>
2022-10-01 15:59:59 -0300

    feat: add loading spinner and empty text on WeatherSidebar component

--- src/components/WeatherSidebar/index.tsx
+++ src/components/WeatherSidebar/index.tsx
@@ -1,4 +1,5 @@
-import { Box, HStack, VStack, Text, Image } from "@chakra-ui/react";
+import { Fragment } from 'react';
+import { Box, HStack, VStack, Center, Text, Image, Spinner } from "@chakra-ui/react";
 import { useRecoilValue } from 'recoil';
 
 import { cityNameState } from 'state/atoms';
@@ -12,32 +13,45 @@ export const WeatherSidebar = ({ isLoading, data }: WeatherSidebarProps) => {
 
 	return (
 		<Box w={{ base: 'full', lg: '350px' }} p={4} bg="blue.800" borderRadius="3xl">
-			{isLoading && <Text>Loading...</Text>}
-			<Text>{cityName}</Text>
+			{isLoading ? (
+				<Center my={10}>
+					<Spinner size="xl" color="blue.400" />
+				</Center>
+			) : (
+				<Fragment>
+					{data.length > 0 ? (
+						<Fragment>
+							<Text>{cityName}</Text>
 
-			{data.map((weatherInfo) => {
-				const { temp_min, temp_max } = weatherInfo.main;
-				const { description, icon } = weatherInfo.weather[0];
-				const day = new Date(weatherInfo.dt_txt).getDay();
+							{data.map((weatherInfo) => {
+								const { temp_min, temp_max } = weatherInfo.main;
+								const { description, icon } = weatherInfo.weather[0];
+								const day = new Date(weatherInfo.dt_txt).getDay();
 
-				return (
-					<HStack key={day} justifyContent="space-between">
-						<HStack>
-							<Image
-								src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-								alt={description}
-								w="60px"
-								h="60px"
-							/>
-							<VStack alignItems="normal" spacing={0}>
-								<Text fontSize="xs">{getWeekDay(day)}</Text>
-								<Text>{description}</Text>
-							</VStack>
-						</HStack>
-						<Text>{`${Math.round(temp_min)}° / ${Math.round(temp_max)}°`}</Text>
-					</HStack>
-				);
-			})}
+								return (
+									<HStack key={day} justifyContent="space-between">
+										<HStack>
+											<Image
+												src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+												alt={description}
+												w="60px"
+												h="60px"
+											/>
+											<VStack alignItems="normal" spacing={0}>
+												<Text fontSize="xs">{getWeekDay(day)}</Text>
+												<Text>{description}</Text>
+											</VStack>
+										</HStack>
+										<Text>{`${Math.round(temp_min)}° / ${Math.round(temp_max)}°`}</Text>
+									</HStack>
+								);
+							})}
+						</Fragment>
+					) : (
+						<Text textAlign="center" my={10}>No weather result.</Text>
+					)}
+				</Fragment>
+			)}
 		</Box>
 	);
 };
